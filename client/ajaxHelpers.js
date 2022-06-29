3101e1bebada3d4151032f9ec5d2995a1e65fa76
Thomas <thomas-g-smith@hotmail.com>
2022-06-29 18:03:32 -0500

    added page refresh when submitting a new player

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,56 +1,48 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = '2206-FTB-ET-WEB-FT';
+const cohortName = "2206-FTB-ET-WEB-FT";
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
-
 export const fetchAllPlayers = async () => {
-    try {
-        const response = await fetch(`${APIURL}/players`);
-        const result = await response.json();
-        if (result.error) throw result.error;
-        return result.data.players;
-      } catch (err) {
-        console.error('Uh oh, trouble fetching players!', err);
-      }
+  try {
+    const response = await fetch(`${APIURL}/players`);
+    const result = await response.json();
+    if (result.error) throw result.error;
+    return result.data.players;
+  } catch (err) {
+    console.error("Uh oh, trouble fetching players!", err);
+  }
 };
 
 export const fetchSinglePlayer = async (playerId) => {
-        const response = await fetch(`${APIURL}/players/${playerId}`);
-        const result = await response.json();
-        let sResult = result.data.player
-        return sResult;
-      
+  const response = await fetch(`${APIURL}/players/${playerId}`);
+  const result = await response.json();
+  let sResult = result.data.player;
+  return sResult;
 };
 
 export const addNewPlayer = async (playerObj) => {
-const response = await fetch(`${APIURL}/players`,{
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(playerObj)
-  
-} 
-
-)
-const result = await response.json()
-console.log(result)
+  const response = await fetch(`${APIURL}/players`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(playerObj),
+  });
+  const result = await response.json();
+  console.log(result);
 };
 export const removePlayer = async (playerId) => {
   fetch(`${APIURL}/players`, {
-    method: 'DELETE',
+    method: "DELETE",
   });
   try {
-    const response = await fetch(
-      `${APIURL}/players/${playerId}`,
-      {
-        method: 'DELETE',
-      }
-    );
+    const response = await fetch(`${APIURL}/players/${playerId}`, {
+      method: "DELETE",
+    });
     const result = await response.json();
     console.log(result);
   } catch (err) {
     console.error(err);
   }
-  };
+};
--- client/renderHelpers.js
+++ client/renderHelpers.js
@@ -1,17 +1,23 @@
-import { fetchAllPlayers, fetchSinglePlayer, addNewPlayer, removePlayer } from './ajaxHelpers';
+import { init } from ".";
+import {
+  fetchAllPlayers,
+  fetchSinglePlayer,
+  addNewPlayer,
+  removePlayer,
+} from "./ajaxHelpers";
 
-const playerContainer = document.getElementById('all-players-container');
-const newPlayerFormContainer = document.getElementById('new-player-form');
+const playerContainer = document.getElementById("all-players-container");
+const newPlayerFormContainer = document.getElementById("new-player-form");
 
 export const renderAllPlayers = (playerList) => {
   // First check if we have any data before trying to render it!
   if (!playerList || !playerList.length) {
-    playerContainer.innerHTML = '<h3>No players to display!</h3>';
+    playerContainer.innerHTML = "<h3>No players to display!</h3>";
     return;
   }
 
   // Loop through the list of players, and construct some HTML to display each one
-  let playerContainerHTML = '';
+  let playerContainerHTML = "";
   for (let i = 0; i < playerList.length; i++) {
     const pup = playerList[i];
     let pupHTML = `
@@ -34,36 +40,34 @@ export const renderAllPlayers = (playerList) => {
   // Now that the HTML for all players has been added to the DOM,
   // we want to grab those "See details" buttons on each player
   // and attach a click handler to each one
-  let detailButtons = [...document.getElementsByClassName('detail-button')];
+  let detailButtons = [...document.getElementsByClassName("detail-button")];
   for (let i = 0; i < detailButtons.length; i++) {
     const button = detailButtons[i];
-    button.addEventListener('click', async () => {
-      let playerId= button.dataset.id
-      let singlePlayer = await fetchSinglePlayer(playerId)
-      console.log(singlePlayer)
-      renderSinglePlayer(singlePlayer)
+    button.addEventListener("click", async () => {
+      let playerId = button.dataset.id;
+      let singlePlayer = await fetchSinglePlayer(playerId);
+      console.log(singlePlayer);
+      renderSinglePlayer(singlePlayer);
 
-      let returnButton = document.getElementById('see-all');
-      returnButton.addEventListener('click', async () => {
-        const allPlayers = await fetchAllPlayers()
-        renderAllPlayers(allPlayers)
-      })
-      
-      
+      let returnButton = document.getElementById("see-all");
+      returnButton.addEventListener("click", async () => {
+        const allPlayers = await fetchAllPlayers();
+        renderAllPlayers(allPlayers);
+      });
     });
   }
-  
-  let deleteButtons = [...document.getElementsByClassName('delete-button')];
-for (let i = 0; i < deleteButtons.length; i++) {
-const button = deleteButtons[i];
-button.addEventListener('click', async () => {
-const kickedPlayer = button.dataset.id
-await removePlayer(kickedPlayer);
-const players = await fetchAllPlayers();
-renderAllPlayers(players);
-console.log('i was clicked')
-});
-} 
+
+  let deleteButtons = [...document.getElementsByClassName("delete-button")];
+  for (let i = 0; i < deleteButtons.length; i++) {
+    const button = deleteButtons[i];
+    button.addEventListener("click", async () => {
+      const kickedPlayer = button.dataset.id;
+      await removePlayer(kickedPlayer);
+      const players = await fetchAllPlayers();
+      renderAllPlayers(players);
+      console.log("i was clicked");
+    });
+  }
 };
 
 export const renderSinglePlayer = (playerObj) => {
@@ -79,7 +83,7 @@ export const renderSinglePlayer = (playerObj) => {
         <p class="pup-title">${playerObj.name}</p>
         <p class="pup-number">#${playerObj.id}</p>
       </div>
-      <p>Team: ${playerObj.team ? playerObj.team.name : 'Unassigned'}</p>
+      <p>Team: ${playerObj.team ? playerObj.team.name : "Unassigned"}</p>
       <p>Breed: ${playerObj.breed}</p>
       <img src="${playerObj.imageUrl}" alt="photo of ${
     playerObj.name
@@ -87,11 +91,9 @@ export const renderSinglePlayer = (playerObj) => {
       <button id="see-all">Back to all players</button>
     </div>
     `;
-    
-    
-    playerContainer.innerHTML = pupHTML;
 
-  };
+  playerContainer.innerHTML = pupHTML;
+};
 
 export const renderNewPlayerForm = () => {
   let formHTML = `
@@ -105,16 +107,19 @@ export const renderNewPlayerForm = () => {
   `;
   newPlayerFormContainer.innerHTML = formHTML;
 
-  let form = document.querySelector('#new-player-form > form');
-  form.addEventListener('submit', async (event) => {
-    // event.preventDefault();
+  let form = document.querySelector("#new-player-form > form");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
     let inputs = form.elements;
     let playerData = {
-    name: inputs['name'].value,
-    breed: inputs['breed'].value
-    }
-    addNewPlayer(playerData)
-    const allPlayers = await fetchAllPlayers()
-        renderAllPlayers(allPlayers)
+      name: inputs["name"].value,
+      breed: inputs["breed"].value,
+    };
+    addNewPlayer(playerData);
+    const players = await fetchAllPlayers();
+    renderAllPlayers(players);
+
+    renderNewPlayerForm();
+    init();
   });
 };
